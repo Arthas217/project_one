60d1e43d9fa87fac108db7b704212820e80941fa
zuocheng <442854399@qq.com>
2020-05-17 13:04:33 +0800

    vst

---
+++ src/main/java/org/com/zlk/leedcode/classic/ValidBST.java
@@ -0,0 +1,23 @@
+package org.com.zlk.leedcode.classic;
+
+import org.com.zlk.company.tree.TreeNode;
+
+public class ValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        // TreeNode节点中value属性 类型是int   [2147483647]这个用例不行。
+        return help(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean help(TreeNode root, Long lower, Long upper) {
+        // 如有一个根节点
+        if (root == null) {
+            return true;
+        }
+        if (root.value >= upper || root.value <= lower) {
+            return false;
+        }
+        return help(root.left, lower, (long) root.value) && help(root.right, (long) root.value, upper);
+    }
+
+}
