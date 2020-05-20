9375af61f9c15ae59b4e6b4a24d7f8083fa0fcb7
zuocheng <442854399@qq.com>
2020-05-20 11:03:16 +0800

    cache

--- src/main/java/org/com/zlk/basic/cache/GuavaCache.java
+++ src/main/java/org/com/zlk/basic/cache/GuavaCache.java
@@ -3,37 +3,32 @@ package org.com.zlk.basic.cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import org.com.zlk.company.toutiao.LRU;
+import com.google.common.util.concurrent.ListenableFuture;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.*;
 
 public class GuavaCache {
 
-    private static Map<String,Integer> service = new HashMap<>();
+    private static ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor) Executors.newSingleThreadExecutor();
 
+    // Guava Cache的使用
+    private static LoadingCache<String, Integer> loadingCache = CacheBuilder.newBuilder()
+            .maximumSize(10000)
+            .expireAfterWrite(10, TimeUnit.SECONDS)
+            .expireAfterAccess(0, TimeUnit.SECONDS)
+            .refreshAfterWrite(0, TimeUnit.SECONDS)
+            .recordStats()
+            .build(new CacheLoader<String, Integer>() {
+                @Override
+                public Integer load(String s) throws Exception {
+                    return 0;
+                }
+            });
 
-    static {
-        // Guava Cache的使用
-        LoadingCache<String, Integer> loadingCache = CacheBuilder
-                .newBuilder()
-                .maximumSize(10000)
-                .expireAfterWrite(10, TimeUnit.SECONDS)
-                .recordStats()
-                .build(new CacheLoader<String, Integer>() {
-                    @Override
-                    public Integer load(String s) throws Exception {
-                        return service.get(s);
-                    }
-                });
-
-    }
-
-
-
-    public static void main(String[] args) {
-        service.put("docId",123);
-
+    public static void main(String[] args) throws ExecutionException {
+        loadingCache.get("key");
+        loadingCache.refresh("key");
     }
 }
