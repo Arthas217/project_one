48a89365fd923f5ea0908bbfd137458b506adba9
zuocheng <442854399@qq.com>
2020-06-02 15:10:33 +0800

    haspathsum

--- src/main/java/org/com/zlk/company/toutiao/BinaryTreeSumN.java
+++ src/main/java/org/com/zlk/company/toutiao/BinaryTreeSumN.java
@@ -5,11 +5,14 @@ import org.com.zlk.datastructure.tree.TreeNode;
 
 /**
  * 给定一个二叉树和一个数字n，判断二叉树中是否有一个路径上的数字之和等于给定的数字n
+ * 路径和定义为从 root 到 leaf 的所有节点的和。
  */
 public class BinaryTreeSumN {
 
+
     /**
-     * 递归
+     * 递归第一个版本
+     * target 给定和的值
      */
     public static boolean hasPathSum(TreeNode root, int target) {
         // 根节点root 、路径值0
@@ -32,9 +35,23 @@ public class BinaryTreeSumN {
     }
 
 
+    public static boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        // 判断叶子节点，sum == 0为依据
+        if (root.left == null && root.right == null && root.val == sum) {
+            return true;
+        }
+        // 遍历节点时 && 对sum做处理
+        return hasPathSum2(root.left, sum - root.val) || hasPathSum2(root.right, sum - root.val);
+    }
+
     public static void main(String[] args) {
         TreeNode treeNode1 = InitTreeNoe.init1();
         boolean hasPathSum = hasPathSum(treeNode1, 22);
+        boolean hasPathSum2 = hasPathSum2(treeNode1, 22);
         System.out.println(hasPathSum);
+        System.out.println(hasPathSum2);
     }
 }
