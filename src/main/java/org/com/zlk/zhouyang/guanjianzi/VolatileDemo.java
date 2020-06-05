05c281bfb3b13ec7a9e0ac9a14555502e8c2779f
zuocheng <442854399@qq.com>
2020-06-05 16:33:09 +0800

    volatile

--- src/main/java/org/com/zlk/zhouyang/guanjianzi/VolatileDemo.java
+++ src/main/java/org/com/zlk/zhouyang/guanjianzi/VolatileDemo.java
@@ -2,13 +2,36 @@ package org.com.zlk.zhouyang.guanjianzi;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- * volatile可见性
- */
 public class VolatileDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        //volatile可见性
+        seeOkByVolatile();
+
+        // volatile原子性(数据操作的一致性)吗？ 不支持
+        atomicByVolatile();
+    }
+
+    private static void atomicByVolatile() throws InterruptedException {
+        ShareData data = new ShareData();
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    data.addPP();
+                }
+            }, String.valueOf(i)).start();
+        }
+        // 这里注意 一般默认后台是2个线程 （主线程、GC线程）
+        while (Thread.activeCount() > 2) {
+            // 该线程的礼让，让其他线程执行
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName() + "------------finally num value：" + data.num);
+    }
+
+    private static void seeOkByVolatile() {
         ShareData data = new ShareData();
         new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + "------------start----------");
@@ -28,7 +51,6 @@ public class VolatileDemo {
         }
         System.out.println(Thread.currentThread().getName() + "---------- session over------------");
         System.out.println(Thread.currentThread().getName() + "---------- num value:------------" + data.num);
-
     }
 }
 
@@ -38,5 +60,9 @@ class ShareData {
 
     public void addNum() {
         this.num = 60;
+    }
+
+    public void addPP() {
+        num++;
     }
 }
