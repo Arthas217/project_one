cf918da5c6dcb4cfc7576a8d2289a3009567f532
zuocheng <442854399@qq.com>
2020-06-11 16:32:57 +0800

    listnode

--- src/main/java/org/com/zlk/datastructure/list/BasicOperationList.java
+++ src/main/java/org/com/zlk/datastructure/list/BasicOperationList.java
@@ -47,4 +47,23 @@ public class BasicOperationList {
             head = head.next;
         }
     }
+
+
+    /**
+     * 判断链表是否存在环
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode l1 = head, l2 = head.next;
+        while (l1 != null && l2 != null && l2.next != null) {
+            if (l1 == l2) {
+                return true;
+            }
+            l1 = l1.next;
+            l2 = l2.next.next;
+        }
+        return false;
+    }
 }
