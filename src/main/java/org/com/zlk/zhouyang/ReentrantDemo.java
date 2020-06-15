d83985084614fb2b1c9f6d36ecf057cace0b715c
zuocheng <442854399@qq.com>
2020-06-15 18:01:53 +0800

    可重入锁

--- src/main/java/org/com/zlk/zhouyang/ReentrantDemo.java
+++ src/main/java/org/com/zlk/zhouyang/ReentrantDemo.java
@@ -9,8 +9,15 @@ public class ReentrantDemo {
 
     public static void main(String[] args) {
         Phone phone = new Phone();
-        // 线程操纵资源类
+        // 线程操纵资源类   synchronized的可重入锁方式
         new Thread(()-> phone.sendSMS(),"t1").start();
         new Thread(()-> phone.sendSMS(),"t2").start();
+
+        // 线程操纵资源类   ReentrantLock的可重入锁方式
+        new Thread(()-> phone.ReentrantGet(),"t3").start();
+        new Thread(()-> phone.ReentrantGet(),"t4").start();
+
+
+
     }
 }
--- src/main/java/org/com/zlk/zhouyang/model/Phone.java
+++ src/main/java/org/com/zlk/zhouyang/model/Phone.java
@@ -1,5 +1,8 @@
 package org.com.zlk.zhouyang.model;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 资源类
  */
@@ -23,4 +26,20 @@ public class Phone {
         System.out.println(Thread.currentThread().getName() + "threadid " + Thread.currentThread().getId() + "\t ###发邮件");
     }
 
+
+
+    Lock lock = new ReentrantLock();
+
+    public void ReentrantGet() {
+        lock.lock();
+        System.out.println(Thread.currentThread().getName() + "threadid " + Thread.currentThread().getId() + "\t ###get()");
+        ReentrantSet();
+        lock.unlock();
+    }
+
+    public void ReentrantSet(){
+        lock.lock();
+        System.out.println(Thread.currentThread().getName() + "threadid " + Thread.currentThread().getId() + "\t ###set()");
+        lock.unlock();
+    }
 }
