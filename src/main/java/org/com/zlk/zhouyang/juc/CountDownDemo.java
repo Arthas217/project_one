c5076a13a1cab8a7552389871183b24b63b0a78a
zuocheng <442854399@qq.com>
2020-06-15 19:55:16 +0800

    conutdown

--- src/main/java/org/com/zlk/zhouyang/juc/CountDownDemo.java
+++ src/main/java/org/com/zlk/zhouyang/juc/CountDownDemo.java
@@ -1,4 +1,47 @@
 package org.com.zlk.zhouyang.juc;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 火箭发射倒计时
+ */
 public class CountDownDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        failCase();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("-------------");
+        correctCase();
+        System.out.println("-------------");
+        //枚举
+        System.out.println(NameEnum.NAME1);
+        System.out.println(NameEnum.NAME1.getCode());
+        System.out.println(NameEnum.NAME1.getMessage());
+    }
+
+    public static void failCase() {
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t 从图书馆离开");
+            }, String.valueOf(i)).start();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t 管理员闭馆");
+    }
+
+    public static void correctCase() {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t 从图书馆离开");
+                countDownLatch.countDown();// 只做减法
+            }, NameEnum.getName(i).getMessage()).start();   //这里使用枚举的方式来解决映射值（值很多）
+        }
+        try {
+            countDownLatch.await();// 保证在上面的6个线程都执行完之后，主线程在执行的动作
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t 管理员闭馆");
+    }
 }
--- src/main/java/org/com/zlk/zhouyang/juc/NameEnum.java
+++ src/main/java/org/com/zlk/zhouyang/juc/NameEnum.java
@@ -1,4 +1,34 @@
 package org.com.zlk.zhouyang.juc;
 
+import lombok.Getter;
+
 public enum NameEnum {
+
+    NAME1(1, "张三"),
+    NAME2(2, "王五"),
+    NAME3(3, "李四"),
+    NAME4(4, "赵一"),
+    NAME5(5, "刘二"),
+    NAME6(6, "左六");
+
+
+    @Getter
+    private int code;
+    @Getter
+    private String message;
+
+    NameEnum(int key, String name) {
+        this.code = key;
+        this.message = name;
+    }
+
+    public static NameEnum getName(int key) {
+        NameEnum[] nameEnums = NameEnum.values();
+        for (NameEnum nameEnum : nameEnums) {
+            if (nameEnum.getCode() == key) {
+                return nameEnum;
+            }
+        }
+        return null;
+    }
 }
