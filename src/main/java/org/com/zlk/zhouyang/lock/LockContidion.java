f65df2ec18288c5e3190e27ac49240fa6b0973a2
zuocheng <442854399@qq.com>
2020-06-16 18:10:07 +0800

    adjust

--- src/main/java/org/com/zlk/zhouyang/lock/LockContidion.java
+++ src/main/java/org/com/zlk/zhouyang/lock/LockContidion.java
@@ -2,37 +2,36 @@ package org.com.zlk.zhouyang.lock;
 
 /**
  * ReentrantLock、 Condition的await/signal
- * 优势 分组唤醒需要唤醒的线程、精确唤醒
+ * 相比Synchronized优势 分组唤醒需要唤醒的线程、精确唤醒
  * 按顺序打印 A -> B -> C
  * 线程同步处理
  */
 public class LockContidion {
 
-    public static final int NUM= 1;
+    // 顺序打印次数
+    public static final int COUNT = 1;
 
     public static void main(String[] args) {
 
         ShareData shareDate = new ShareData();
 
-        //线程AA打印5次 BB打印10次 CC打印15次
+        //线程A打印5次 BB打印10次 CC打印15次
         new Thread(() -> {
-            for (int i = 0; i < NUM; i++) {
-                shareDate.print5();
+            for (int i = 0; i < COUNT; i++) {
+                shareDate.ThreadOnePrint();
             }
-        }, "AA").start();
-
-        new Thread(() -> {
-            for (int i = 0; i < NUM; i++) {
-                shareDate.print10();
-            }
-        }, "BB").start();
+        }, "A").start();
 
         new Thread(() -> {
-            for (int i = 0; i < NUM; i++) {
-                shareDate.print15();
+            for (int i = 0; i < COUNT; i++) {
+                shareDate.ThreadTwoPrint();
             }
-        }, "CC").start();
+        }, "B").start();
 
-
+        new Thread(() -> {
+            for (int i = 0; i < COUNT; i++) {
+                shareDate.ThreadThreePrint();
+            }
+        }, "C").start();
     }
 }
--- src/main/java/org/com/zlk/zhouyang/lock/ShareData.java
+++ src/main/java/org/com/zlk/zhouyang/lock/ShareData.java
@@ -1,8 +1,11 @@
 package org.com.zlk.zhouyang.lock;
 
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Handler;
 
 /**
  * 共享资源
@@ -10,31 +13,37 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ShareData {
 
-    private int symbol = 1;// 1 A 2 B 3C  标志位
+    // 每个线程打印次数
+    private static final int PRINT_COUNT = 2;
+
+    private int symbol = 1; // 1 A线程 2 B线程 3 C线程  标志位
 
     // 多线程操作资源类竞争，加锁（1把锁）
     private Lock lock = new ReentrantLock();
 
-    // 3把备用钥匙（分组唤醒）
+    // 分组唤醒(3把备用钥匙)
     Condition condition1 = lock.newCondition();
     Condition condition2 = lock.newCondition();
     Condition condition3 = lock.newCondition();
 
     /**
-     * 线程AA
+     * 打印
      */
-    public void print5() {
+    public void ThreadOnePrint() {
         lock.lock();
         try {
             // 避免虚假唤醒
-            while (symbol != 1) {//不是A
-                condition1.await();  //A等待
+            while (symbol != 1) {
+                // 不是A线程 && A线程等待
+                condition1.await();
             }
-            for (int i = 0; i < 1; i++) {
+            // A线程干活
+            for (int i = 0; i < PRINT_COUNT; i++) {
                 System.out.println(Thread.currentThread().getName() + "\t " + i);
             }
+            // 通知B线程，唤醒线程B
             symbol = 2;
-            condition2.signal();// 唤醒线程
+            condition2.signal();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -42,16 +51,13 @@ public class ShareData {
         }
     }
 
-    /**
-     * 线程BB
-     */
-    public void print10() {
+    public void ThreadTwoPrint() {
         lock.lock();
         try {
             while (symbol != 2) {
                 condition2.await();
             }
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < PRINT_COUNT; i++) {
                 System.out.println(Thread.currentThread().getName() + "\t " + i);
             }
             symbol = 3;
@@ -63,16 +69,13 @@ public class ShareData {
         }
     }
 
-    /**
-     * 线程CC
-     */
-    public void print15() {
+    public void ThreadThreePrint() {
         lock.lock();
         try {
             while (symbol != 3) {
                 condition3.await();
             }
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < PRINT_COUNT; i++) {
                 System.out.println(Thread.currentThread().getName() + "\t " + i);
             }
             symbol = 1;
