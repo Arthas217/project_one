7db22f14a6ea1561a3699fb07108566cd02d5e5f
zuocheng <442854399@qq.com>
2020-07-10 11:43:43 +0800

    1

--- src/main/java/org/com/zlk/company/toutiao/BinaryTreeAllPath.java
+++ src/main/java/org/com/zlk/company/toutiao/BinaryTreeAllPath.java
@@ -28,7 +28,7 @@ public class BinaryTreeAllPath {
             if ((root.left == null) && (root.right == null) && target == 0)  // 所有路径中值满足target的路径
                 paths.add(path);  // 把路径加入到答案中
             else {
-                path += "->";  // 当前节点不是叶子节点，继续递归遍历
+                path += ">>";  // 当前节点不是叶子节点，继续递归遍历
                 construct_paths(root.left, path, paths, target);
                 construct_paths(root.right, path, paths, target);
             }
