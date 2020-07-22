2a16bf67ee1c1a037d3d2137f88c979408162285
zuocheng <442854399@qq.com>
2020-07-22 18:13:21 +0800

    回文最长字符串

--- src/main/java/org/com/zlk/leedcode/StrSolution.java
+++ src/main/java/org/com/zlk/leedcode/StrSolution.java
@@ -79,6 +79,48 @@ public class StrSolution {
         return result;
     }
 
+    // 5. 最长回文子串 暴力
+    //动态规划https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-dong-tai-gui-hua-by-liweiwei1419/
+    public static String longestPalindrome2(String s) {
+        int len = s.length();
+        // 单字符 || null
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否为回文子串 这里都是闭区间,i <= j ，因此，只需要填这张表格对角线以上的部分
+        boolean[][] dp = new boolean[len][len];
+        char[] ch = s.toCharArray();
+        // dp对角线上表示只有一个字符
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        // 公式 dp[i][j] = (s[i] == s[j]) 头尾字符是否相等 && dp[i + 1][j - 1]
+        for (int j = 1; j < len; j++) {
+            //dp的每一行填值 （true、false）
+            for (int i = 0; i < j; i++) {
+                if (ch[i] != ch[j]) {
+                    dp[i][j] = false;
+                } else {
+                    // dp[i + 1][j - 1] 考虑边界情况,当不构成区间时，即j - 1 - (i + 1) + 1 < 2  即j - i  < 3 。
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                // 选择满足dp值=true的最大len并把位置记录下来
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+
     public static void main(String[] args) {
 //        String str = "abcbcbb";
 //        System.out.println(lengthOfLongestSubstring(str));
@@ -86,7 +128,8 @@ public class StrSolution {
 //        String st = "A man, a plan, a canal: Panama";
 //        System.out.println(isPalindrome(st));
 
-        String str = "babad";
+        String str = "amanaplanacanalpanama";
         System.out.println(longestPalindrome(str));
+        System.out.println(longestPalindrome2(str));
     }
 }
