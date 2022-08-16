0e26718bb24779cbe76b23d77086e8ff57d2ad47
zuocheng <442854399@qq.com>
2022-08-16 17:04:43 +0800

    2022-8-16-time

--- src/main/java/org/com/zlk/java8/time/timeDemo.java
+++ src/main/java/org/com/zlk/java8/time/timeDemo.java
@@ -3,6 +3,7 @@ package org.com.zlk.java8.time;
 
 import java.text.SimpleDateFormat;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
@@ -99,6 +100,83 @@ public class timeDemo {
         System.out.println("一年前的日期 : " + previousYear);
         LocalDate nextYear = today2.plus(1, ChronoUnit.YEARS);
         System.out.println("一年后的日期:" + nextYear);
+
+        //Java 8的Clock时钟类
+        //增加了一个Clock时钟类用于获取当时的时间戳，或当前时区下的日期时间信息。
+        //以前用到System.currentTimeInMillis()和TimeZone.getDefault()的地方都可用Clock替换。
+        // Returns the current time based on your system clock and set to UTC.
+        Clock clock = Clock.systemUTC();
+        System.out.println("Clock : " + clock.millis());
+
+        // Returns time based on system clock zone
+        Clock defaultClock = Clock.systemDefaultZone();
+        System.out.println("Clock : " + defaultClock.millis());
+
+        //在Java 8中获取当前的时间戳
+        //时间戳信息里同时包含了日期和时间，实际上Instant类确实等同于 Java 8之前的Date类，可互相转换
+        Instant timestamp = Instant.now();
+        System.out.println("What is value of this instant " + timestamp.toEpochMilli());
+
+        //Java 8中如何使用预定义的格式化工具去解析或格式化日期
+        String dayAfterTommorrow = "20220818";
+        LocalDate formatted = LocalDate.parse(dayAfterTommorrow, DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(dayAfterTommorrow + "  格式化后的日期为:  " + formatted);
+
+        // Java判断日期是早于还是晚于另一个日期,LocalDate类有两类方法isBefore()和isAfter()用于比较日期。
+        LocalDate today3 = LocalDate.now();
+        System.out.println("当前时间:" + today3);
+        LocalDate tomorrow = today3.plus(1, ChronoUnit.DAYS);
+        if (tomorrow.isAfter(today3)) {
+            System.out.println("之后的日期:" + tomorrow);
+        }
+
+        LocalDate yesterday = today3.minus(1, ChronoUnit.DAYS);
+        if (yesterday.isBefore(today3)) {
+            System.out.println("之前的日期:" + yesterday);
+        }
+
+        //Java 8中处理时区
+        //现在有一系列单独的类如ZoneId来处理特定时区，ZoneDateTime类来表示某时区下的时间
+        //Java 8以前都是 GregorianCalendar类来做的。
+        ZoneId america = ZoneId.of("America/New_York");
+        LocalDateTime localtDateAndTime = LocalDateTime.now();
+        ZonedDateTime dateAndTimeInNewYork = ZonedDateTime.of(localtDateAndTime, america);
+        System.out.println("Current date and time in a particular timezone : " + dateAndTimeInNewYork);
+
+        // 如何表示信用卡到期这类固定日期，答案就在YearMonth,另外实例的lengthOfMonth()方法可以返回当月的天数
+        YearMonth currentYearMonth = YearMonth.now();
+        System.out.printf("Days in month year %s: %d%n", currentYearMonth, currentYearMonth.lengthOfMonth());
+        YearMonth creditCardExpiry = YearMonth.of(2019, Month.FEBRUARY);
+        System.out.printf("Your credit card expires on %s %n", creditCardExpiry);
+
+        //Java 8中检查闰年
+        LocalDate today4 = LocalDate.now();
+        if (today4.isLeapYear()) {
+            System.out.println("This year is Leap year");
+        } else {
+            System.out.println(today4 + " is not a Leap year");
+        }
+
+        // 计算两个日期之间的天数、周数或月数,用java.time.Period类
+//        LocalDate today5 = LocalDate.now();
+//        LocalDate java8Release = LocalDate.of(2018, 12, 14);
+//        System.out.println(java8Release);
+//        System.out.println(today5);
+//        Period periodToNextJavaRelease = Period.between(java8Release,today5);
+//        System.out.println("Months left between today and Java 8 release : " + periodToNextJavaRelease.getMonths());
+
+
+        //日期转字符串
+        LocalDateTime date3 = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String str = date3.format(dateTimeFormatter1);
+        System.out.println("日期转换为字符串:" + str);
+
+        //字符串转日期
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDate localDate = LocalDate.parse(str, dateTimeFormatter2);
+        System.out.println("字符串转为日期:" + localDate);
+
     }
 
     private static void testCalendar() {
