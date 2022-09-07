13acf8f3f269a2933907e9807895118604400e32
zuocheng <442854399@qq.com>
2022-09-07 16:37:45 +0800

    2022-9-5 jsonarray

--- src/main/java/org/com/zlk/json/JSONArrayDemo.java
+++ src/main/java/org/com/zlk/json/JSONArrayDemo.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 /**
  * @Author 会游泳的蚂蚁
- * @Description: JSONArray遍历
+ * @Description: JSONArray遍历 put，get顺序一致
  * @Date 2022/8/2 09:33
  */
 public class JSONArrayDemo {
@@ -20,23 +20,24 @@ public class JSONArrayDemo {
         for (int i = 0; i < jsonArr.size(); i++) {
             JSONObject jsonObject = jsonArr.getJSONObject(i);
             String name = jsonObject.getString("name");
-            Integer age = (Integer) jsonObject.get("age");
-            System.out.println("name:" + name + ";age:" + age);
+            System.out.println("name:" + name);
         }
     }
+
     //2、JSONArray遍历stream流
     public static void method2(JSONArray dists) {
         System.out.println("-----2---------");
-        Set<String> list=new HashSet<>();
-        dists.stream().filter(Objects::nonNull).forEach(dist->{
-            String name = (String)((JSONObject) dist).get("name");
-            list.add(name);
+        Set<String> set = new HashSet<>();
+        dists.stream().filter(Objects::nonNull).forEach(dist -> {
+            String name = (String) ((JSONObject) dist).get("name");
+            set.add(name);
         });
+        System.out.println(set);
     }
 
 
     public static void main(String[] args) {
-        String str = "[{\"name\":\"7190214\"},{\"name\":\"7190208\"},{\"name\":\"6811009\"},{\"name\":\"7040177\"},{\"name\":\"7070152\"},{\"name\":\"7060188\"},{\"name\":\"7050156\"},{\"name\":\"7020169\"},{\"name\":\"7010202\"}]";
+        String str = "[{\"name\":\"a\"},{\"name\":\"b\"},{\"name\":\"c\"},{\"name\":\"d\"},{\"name\":\"e\"},{\"name\":\"f\"},{\"name\":\"g\"},{\"name\":\"h\"},{\"name\":\"i\"}]";
         JSONArray array = JSONArray.parseArray(str);
         System.out.println(array);
         method1(array);
