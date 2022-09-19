4a50456a358c0ef730d859459019a458c9601faf
zuocheng <442854399@qq.com>
2022-09-19 08:49:23 +0800

    2022-9-19 DI

---
+++ src/main/java/org/com/zlk/spring/zhujie/ConstructorBasedInjection.java
@@ -0,0 +1,28 @@
+package org.com.zlk.spring.zhujie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author 会游泳的蚂蚁
+ * @Description: 基于构造函数的依赖注入
+ * 主要优点是可以将需要注入的字段声明为final， 使得它们会在类实例化期间被初始化，这对于所需的依赖项很方便。
+ * @Date 2022/9/19 08:45
+ */
+@Component
+public class ConstructorBasedInjection {
+
+    /**
+     * 这里也可以是引用对象
+     */
+    private final String injectedBean;
+
+    /**
+     * 类构造函数被标注为@Autowired，并包含了许多与要注入的对象相关的参数。
+     * @param injectedBean
+     */
+    @Autowired
+    public ConstructorBasedInjection(String injectedBean) {
+        this.injectedBean = injectedBean;
+    }
+}
