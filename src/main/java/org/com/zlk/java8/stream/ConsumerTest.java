bcf4429fd2d974ae35f6101bb3ff77cf388daf43
zuocheng <442854399@qq.com>
2023-04-18 18:34:36 +0800

    2023-04-18-chxg-consumer

--- src/main/java/org/com/zlk/java8/stream/ConsumerTest.java
+++ src/main/java/org/com/zlk/java8/stream/ConsumerTest.java
@@ -6,23 +6,27 @@ import java.util.function.Consumer;
 
 /**
  * @Author 会游泳的蚂蚁
- * @Description:
+ * @Description: 内置Consumer函数式接口---代表接受单个输入参数并没有返回值的操作。
  * @Date 2021/12/28 09:19
  */
 public class ConsumerTest {
 
     /**
-     * Lambda是Consumer中accept方法的实现
+     * 一个接受单个参数但没有返回值的Consumer
+     * Lambda表达式是Consumer中accept方法的实现
      */
-    private static Consumer<Integer> consumer = (Integer i) -> System.out.print(i + " ");
+    private static Consumer<Integer> consumer = (value) -> System.out.println(value + "");
 
-    public static <T> void forEachFuntion(List<T> list, Consumer<T> c) {
-        for (T num : list) {
-            c.accept(num);
+    public static <T> void forEachFuntion(List<T> list, Consumer<T> consumer) {
+        for (T t : list) {
+            // accept方法的作用是将该函数应用于给定的参数。
+            consumer.accept(t);
         }
     }
 
     public static void main(String[] args) {
-        forEachFuntion(Arrays.asList(1, 2, 3, 4, 5), consumer);
+        //将每个元素作为参数传递给 consumer 进行处理
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        forEachFuntion(list, consumer);
     }
 }
