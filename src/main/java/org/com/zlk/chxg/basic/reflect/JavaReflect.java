49dc9bbe8b7dff8f8a4d069c4ff7c09799d0c615
zuocheng <442854399@qq.com>
2023-04-19 23:06:03 +0800

    2023-04-18-chxg-cache

--- src/main/java/org/com/zlk/chxg/basic/reflect/JavaReflect.java
+++ src/main/java/org/com/zlk/chxg/basic/reflect/JavaReflect.java
@@ -5,41 +5,39 @@ import java.lang.reflect.Method;
 
 /**
  * @Author 会游泳的蚂蚁
- * @Description: 源码分析（todo）
- * https://www.cnblogs.com/yougewe/p/10125073.html
+ * @Description: java反射原理 https://www.cnblogs.com/yougewe/p/10125073.html
  * @Date 2022/11/8 10:03
  */
 public class JavaReflect {
 
+    public void sayHello(String word) {
+        System.out.println("你好," + word);
+    }
+
     public static void main(String[] args) {
         try {
-            // 1. 使用外部配置的实现，进行动态加载类
-            JavaReflect test = (JavaReflect)Class.forName("org.com.zlk.chxg.basic.reflect.JavaReflect").newInstance();
-            test.sayHello("call directly");
-
-            // 2. 根据配置的函数名，进行方法调用（不需要通用的接口抽象）
+            // 使用外部配置的实现，进行动态加载类
+            JavaReflect javaReflect = (JavaReflect) Class.forName("org.com.zlk.chxg.basic.reflect.JavaReflect").newInstance();
+            javaReflect.sayHello("直接调用sayHello方法");
+            // 根据配置的函数名，进行方法调用（不需要通用的接口抽象）
             Object t2 = new JavaReflect();
             Method method = t2.getClass().getDeclaredMethod("sayHello", String.class);
-            method.invoke(t2, "method invoke");
-
+            method.invoke(t2, "invoke调用sayHello方法");
             for (String arg : args) {
-                System.out.println("打印JavaReflect类main函数的参数"+arg);
+                System.out.println("打印JavaReflect类main函数的参数值" + arg);
             }
-
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-        } catch (NoSuchMethodException e ) {
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
     }
 
-    public void sayHello(String word) {
-        System.out.println("hello," + word);
-    }
+
 }
