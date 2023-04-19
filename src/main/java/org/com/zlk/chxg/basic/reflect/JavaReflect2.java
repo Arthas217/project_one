8a5be2135bf1ffeabae79339152e6340b9b472d2
zuocheng <442854399@qq.com>
2023-04-19 14:02:47 +0800

    2023-04-18-chxg-reflect

--- src/main/java/org/com/zlk/chxg/basic/reflect/JavaReflect2.java
+++ src/main/java/org/com/zlk/chxg/basic/reflect/JavaReflect2.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Author 会游泳的蚂蚁
@@ -21,8 +23,9 @@ public class JavaReflect2 {
             //成员变量
 //            testFieldReflect();
             //成员方法
-            testMethodReflect();
-//            testFXReflect();
+//            testMethodReflect();
+            // 通过反射越过泛型检查
+            testNoPassGenericityByReflect();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -133,8 +136,6 @@ public class JavaReflect2 {
     }
 
 
-
-
     private static void testMethodReflect() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         // 加载Class对象
         Class<?> stuClass = Class.forName("org.com.zlk.chxg.basic.reflect.Student");
@@ -168,36 +169,28 @@ public class JavaReflect2 {
         System.out.println("show4方法返回值：" + result2);
 
 
-        //测试主方法main
-        //1、获取JavaReflect对象的字节码
+        System.out.println("***************获取类JavaReflect中的main()方法******************");
+        // 主方法main 获取JavaReflect对象的字节码
         Class clazz = Class.forName("org.com.zlk.chxg.basic.reflect.JavaReflect");
-        //2、获取main方法
-        Method methodMain = clazz.getMethod("main", String[].class);//第一个参数：方法名称，第二个参数：方法形参的类型，
-        //3、调用main方法
-        // methodMain.invoke(null, new String[]{"a","b","c"});
-        //第一个参数，对象类型，因为方法是static静态的，所以为null可以
-        //第二个参数是String数组，这里要注意在jdk1.4时是数组，jdk1.5之后是可变参数
+        // 获取main方法,其中第一个参数：方法名称，第二个参数：方法形参的类型，
+        Method methodMain = clazz.getMethod("main", String[].class);
+        // 调用main方法,第一个参数，对象类型，因为方法是static静态的，所以为null可以;第二个参数是String数组，这里要注意在jdk1.4时是数组，jdk1.5之后是可变参数
+//        Object inv = methodMain.invoke(null, new String[]{"a", "b", "c"});//会报异常
+        Object inv = methodMain.invoke(null, (Object) new String[]{"a", "b", "c"});//方式一
+//        Object inv = methodMain.invoke(null, new Object[]{new String[]{"a","b","c"}});//方式二
+        System.out.println("调用main方法的返回值：" + inv);
 
-        //这里拆的时候将  new String[]{"a","b","c"} 拆成3个对象。。。所以需要将它强转。
-        methodMain.invoke(null, (Object) new String[]{"a", "b", "c"});//方式一
-        // methodMain.invoke(null, new Object[]{new String[]{"a","b","c"}});//方式二
     }
 
 
 
-
-
-    /**
-     * 通过反射越过泛型检查
-     */
-    private static void testFXReflect() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        ArrayList<String> strList = new ArrayList<>();
-        strList.add("aaa");
-        strList.add("bbb");
-
-        Class<? extends ArrayList> aClass = strList.getClass();
+    private static void testNoPassGenericityByReflect() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+//        List<String> strList = Arrays.asList("aa", "bb");// java.util.Arrays$ArrayList.add(java.lang.Object)
+        List<String> strList = new ArrayList<>();
+        Class<? extends List> aClass = strList.getClass();
         Method m = aClass.getDeclaredMethod("add", Object.class);
-        m.invoke(strList, 100);
+        Object obj = m.invoke(strList, 100);
         System.out.println(strList);
+        System.out.println(obj);
     }
 }
